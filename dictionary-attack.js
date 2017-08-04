7472953d79b383d5230146fd1c5d453314924197
allyzitzka <allykitkaat@gmail.com>
2017-08-04 09:36:02 -0700

    Update dictionary-attack.js

--- dictionary-attack.js
+++ dictionary-attack.js
@@ -1,6 +1,6 @@
 var wordsList = [];
 
-var check = false
+
 
 function init() {
   // Load the words from the dictionary text file to wordsList
@@ -17,6 +17,7 @@ window.onload = init;
 /* ADD YOUR CODE BELOW */
 
 function checkPassword() {
+  var check = false
   var pass = document.getElementById("pw").value;
   for (var i = 0; i < wordsList.length; i++) {
      if (pass == wordsList[i]) {
